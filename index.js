eaa750e3eccfdde8741b66489fda189a02a33d49
Bharath Kumar N <you@example.com>
2020-08-29 13:46:32 +0530

    crud-grapql api

--- index.js
+++ index.js
@@ -24,20 +24,6 @@ app.use("/graphql/product", graphqlHTTP({ schema: productSchema, graphiql: true}
 app.use("/graphql/order", graphqlHTTP({ schema: orderSchema, graphiql: true}));
 app.use("/graphql/orderdetails", graphqlHTTP({ schema: orderdetailsSchema, graphiql: true}));
 app.use("/graphql/payment", graphqlHTTP({ schema: paymentSchema, graphiql: true}));
-// app.listen(4001, () => {
-//         console.log("GraphQL server running at http://localhost:4001/graphql .");
-//         console.log("API Routes ");
-//         console.log("http://localhost:4001/graphql/employee ");
-//         console.log("http://localhost:4001/graphql/customer ");
-//         console.log("http://localhost:4001/graphql/productline ");
-//         console.log("http://localhost:4001/graphql/office ");
-//         console.log("http://localhost:4001/graphql/product ");
-//         console.log("http://localhost:4001/graphql/order ");
-//         console.log("http://localhost:4001/graphql/orderdetails ");
-//         console.log("http://localhost:4001/graphql/payment ");
-// });
-
-
 app.listen(port, host, function(){
         console.log("GraphQL server running at http://localhost:4001/graphql .");
         console.log("API Routes ");

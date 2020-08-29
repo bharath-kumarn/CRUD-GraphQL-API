534a2567a29481e4c0538a94f7b07e66cd7a7534
Bharath Kumar N <you@example.com>
2020-08-29 13:30:10 +0530

    crud-grapql api

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const orderdetailsSchema = require('./schema/orderdetails');
 const paymentSchema = require('./schema/payment');
 const  app  =  express();
 
+const host = '0.0.0.0';
+// const host = 'localhost';
+const port = process.env.PORT || 4001;
+
+
 app.use("/graphql/customer", graphqlHTTP({ schema: customerSchema, graphiql: true}));
 app.use("/graphql/employee", graphqlHTTP({ schema: employeeSchema, graphiql: true}));
 app.use("/graphql/productline", graphqlHTTP({ schema: productlineSchema, graphiql: true}));
@@ -19,7 +24,21 @@ app.use("/graphql/product", graphqlHTTP({ schema: productSchema, graphiql: true}
 app.use("/graphql/order", graphqlHTTP({ schema: orderSchema, graphiql: true}));
 app.use("/graphql/orderdetails", graphqlHTTP({ schema: orderdetailsSchema, graphiql: true}));
 app.use("/graphql/payment", graphqlHTTP({ schema: paymentSchema, graphiql: true}));
-app.listen(4001, () => {
+// app.listen(4001, () => {
+//         console.log("GraphQL server running at http://localhost:4001/graphql .");
+//         console.log("API Routes ");
+//         console.log("http://localhost:4001/graphql/employee ");
+//         console.log("http://localhost:4001/graphql/customer ");
+//         console.log("http://localhost:4001/graphql/productline ");
+//         console.log("http://localhost:4001/graphql/office ");
+//         console.log("http://localhost:4001/graphql/product ");
+//         console.log("http://localhost:4001/graphql/order ");
+//         console.log("http://localhost:4001/graphql/orderdetails ");
+//         console.log("http://localhost:4001/graphql/payment ");
+// });
+
+
+app.listen(port, host, function(){
         console.log("GraphQL server running at http://localhost:4001/graphql .");
         console.log("API Routes ");
         console.log("http://localhost:4001/graphql/employee ");
@@ -30,4 +49,4 @@ app.listen(4001, () => {
         console.log("http://localhost:4001/graphql/order ");
         console.log("http://localhost:4001/graphql/orderdetails ");
         console.log("http://localhost:4001/graphql/payment ");
-});
+       });
